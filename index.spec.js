b352bf69cf26b83395f84d5bd29d8476c3b03323
Rayyan Borg <42804334+MrRayyanBorg@users.noreply.github.com>
2021-04-28 11:10:22 +0100

    Tests and Assertions

---
+++ index.spec.js
@@ -0,0 +1,34 @@
+const { minVal, fizzBuzz, timesTwo } = require('./index');
+test('passes when value is NaN', () => {
+    expect(NaN).toBeNaN();
+    expect(1).not.toBeNaN();
+    expect(typeof NaN).toBe('number');
+});
+
+describe('Testing function minVal()', () => {
+    it('Should return a value of data type number', () => {
+        expect(typeof minVal([1,2,34,0,-12])).toBe('number');
+        expect(minVal([1,2,34,0,-12])).not.toBeInstanceOf(Object);
+    });
+    it('Should get a minimal value from an array', () => {
+        expect(minVal([1,5,13,-10,-12])).toBe(-12);
+    });
+});
+
+describe('Testing function timesTwo()', () => {
+    it('returns an array where numbers are doubled', () => {
+        expect(timesTwo([1,2])).toEqual([2,4]);
+    });
+});
+
+describe('Testing function fizzBuzz()', () => {
+    it('returns an array of the same length as a parameter', () => {
+        expect(fizzBuzz(10)).toHaveLength(4);
+    });
+    it('returns an array with some fizz, buzz and fizzbuzz', () => {
+        expect(fizzBuzz(3)).toContain('fizz');
+        expect(fizzBuzz(30)).toContain('fizzbuzz');
+    })
+});
+
+
